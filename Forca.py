a63f59b62b57b740b6c053926ac352f0dc689c3c
Arthur Guilherme <arthuralmeidags@gmail.com>
2023-10-01 11:26:25 -0300

    adiciona arquivo palavra e muda o jogo na versão 2.0

--- Forca.py
+++ Forca.py
@@ -1,34 +1,58 @@
-
+import random
 
 def jogar_forca():
         print("************************************")
         print("* Bem vindo ao jogo de forca *")
         print("************************************")
 
-        palavra_secreta = "Banana"
-        letras_acertadas = ["_","_","_","_","_","_"]
+        arquivo =  open("palavra.txt", "r")
+        palavras = []
+        for linha in arquivo:
+                linha =  linha.strip()
+                palavras.append(linha)
+
+        arquivo.close()
+
+        numero =  random.randrange(0,len(palavras))
+        palavra_secreta = palavras[numero].upper()  #colocando a palara em maiusculo
+
+
+        letras_acertadas = ["_" for letra in palavra_secreta]
+
+
         enforcou = False
         acertou =  False
+        erros = 0
 
 
         while(not enforcou and not acertou):
                 chute = input("qual a letra? ")
-                chute = chute.strip()
+                chute = chute.strip().upper()
 
-                index = 0
-                for letra in palavra_secreta:
-                        if(chute.upper() == letra.upper()):
-                             letras_acertadas[index] = letra
-                        index = index + 1
-                        print(letras_acertadas)
+                if(chute in palavra_secreta):
+                        index = 0
+                        for letra in palavra_secreta:
+                                if(chute == letra):
+                                        letras_acertadas[index] = letra
+
+                                index += 1
+
+                else :
+                     erros += 1
+
+                enforcou  =  erros == 6
+                acertou =  "_"  not in letras_acertadas
+
+                print(letras_acertadas)
 
 
+        if(acertou):
+                print("parabens, vc ganhou!!!")
 
+        else:
+                print("vc é burrão")
 
-
-
-print("Fim do jogo")
-
+        print("Fim do jogo")
 
 if(__name__=="__main__"):
         jogar_forca()
